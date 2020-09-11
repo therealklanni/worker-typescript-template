e844a2b2d2495112547f4e08ca7f35b8e851bc1f
Kevin Lanni <therealklanni@gmail.com>
2020-09-11 13:38:32 -0700

    Include JS files in lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path')
-const webpack = require('webpack')
 
 const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
   output: {
     filename: `worker.${mode}.js`,
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist')
   },
   mode,
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
-    plugins: [],
+    plugins: []
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.tsx?$/u,
         loader: 'ts-loader',
         options: {
-          transpileOnly: true,
-        },
-      },
-    ],
-  },
+          transpileOnly: true
+        }
+      }
+    ]
+  }
 }
